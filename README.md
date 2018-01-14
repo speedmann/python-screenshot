2911a1af9a17becb354cd848c08af4163705411c
speedmann <speedmann@speedmann.de>
2018-01-14 22:33:32 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,13 @@
+# Simple python script to screenshot a website
+This script will screenshot a website using python / selenium
+You'll need google chrome headless for this to work
+All python requirements are in `requirements.txt`
+
+You also need the googlewebdriver 
+apt-get install chromium-chromedriver
+
+`pip install -r requirements.txt`
+
+just run the script with 
+`python main.py`
+
---
+++ main.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import time
+import imageio
+from datetime import datetime
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+#options.add_argument('window-size=1200x600')
+options.add_argument('window-size=800x600')
+driver = webdriver.Chrome(chrome_options=options)
+
+
+
+
+
+def make_shot():
+  driver.get('http://cryptomaps.org/') # whatever reachable url
+  minute_start = datetime.now().strftime('%H')  
+  minute_now = datetime.now().strftime('%H')  
+  now = datetime.now().strftime('%Y-%m-%d-%H')
+  out='movie' + now + '.gif'
+  with imageio.get_writer(out, mode='I', subrectangles=True) as writer:
+    while minute_now == minute_start:
+      print(minute_now)
+      minute_now = datetime.now().strftime('%H')
+      driver.implicitly_wait(20)
+      driver.save_screenshot('screen.png')   #screen.png is a big red rectangle :)
+      image = imageio.imread('screen.png')
+      writer.append_data(image)
+      driver.refresh()
+      time.sleep(60)
+      print('sleep10')
+
+
+while True:
+  make_shot()
+  print('change min')
+
+driver.quit()
+
+print "png file created"

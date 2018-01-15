c3df043f61202a57d5282fb7e001fc5327d2fb69
speedmann <speedmann@speedmann.de>
2018-01-15 21:15:55 +0100

    Automatically link the current image to a static filename

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 import time
-import imageio
+import imageio, os
 from datetime import datetime
 
 
 loop = 0 # Loop indefinetly, Change to another int value to set number of loops (e.g. 1 only loops once)
 duration = 1.0 # Have every frame displayed for 1 second
+path = './' # where to store the images e.g. /var/www
 
 
 
@@ -25,18 +26,25 @@ def make_shot():
   minute_start = datetime.now().strftime('%H')  
   minute_now = datetime.now().strftime('%H')  
   now = datetime.now().strftime('%Y-%m-%d-%H')
-  out='movie' + now + '.gif'
+  #out='movie' + now + '.gif'
+  out=path + 'movie' + now + '.gif'
+  try:
+    os.symlink(out, path + 'current.gif')
+  except OSError:
+    os.unlink(path + 'current.gif')
+    os.symlink(out, path + 'current.gif')
+    
   with imageio.get_writer(out, mode='I', subrectangles=True, duration=duration, loop=loop) as writer:
     while minute_now == minute_start:
       print(minute_now)
       minute_now = datetime.now().strftime('%H')
       driver.implicitly_wait(20)
-      driver.save_screenshot('screen.png')   #screen.png is a big red rectangle :)
+      driver.save_screenshot('screen.png') 
       image = imageio.imread('screen.png')
       writer.append_data(image)
       driver.refresh()
       time.sleep(60)
-      print('sleep10')
+      print('sleep 60 secods')
 
 
 while True:
